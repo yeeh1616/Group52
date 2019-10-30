874f16f5be08db8aa1fbb4fad6689489a1038e72
Ruben Ollesch <ruben.ollesch@pm.me>
2019-10-30 15:28:36 +0100

    Add new dashboard design

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,10 @@
 from eca import *
-import eca.http
 import json
+import eca.http
+import datetime
+import textwrap
+import pprint
+import re
 
 # This function will be called to set up the HTTP server
 def add_request_handlers(httpd):
@@ -15,20 +19,32 @@ def add_request_handlers(httpd):
     httpd.add_content('/style/', 'template_static/style')
 
 
+
 # binds the 'setup' function as the action for the 'init' event
 # the action will be called with the context and the event
 @event('init')
 def setup(ctx, e):
     ctx.tweetList = []
-    ctx.f = open("dashboard_static/sports.json", "r")
-
+    ctx.f = open("data/sports.json", "r")
     ctx.count = 0
     fire('sample', {'previous': 0.0})
+    ctx.words = {}
+    
+# simple word splitter
+pattern = re.compile('\W+')
 
-
-# define a normal Python function
 def clip(lower, value, upper):
     return max(lower, min(value, upper))
+
+# sample stopword list, needs to be much more sophisticated
+stopwords = ['the', 'for', 'and', 'http', 'you', 'what', 'why', 'where', 'are', 'not', 'www', 'com']
+
+def words(message):
+    result = pattern.split(message)
+    result = map(lambda w: w.lower(), result)
+    result = filter(lambda w: w not in stopwords, result)
+    result = filter(lambda w: len(w) > 2, result)
+    return result
 
 @event('sample')
 def generate_sample(ctx, e):
@@ -39,20 +55,35 @@ def generate_sample(ctx, e):
             tweet = json.loads(line)
         else:
             return
-    except:
-        print("Non-json.")
-        emit('message', {
-            'text': "{}".format('Error')
-        })
-        fire('sample', {'previous': 'Error'}, delay=0.05)
-    else:
-        ctx.tweetList.append(tweet)
-        print(tweet['text'])
-        # emit('message', {
-        #     'text': "Tweet: {}".format(tweet['text'])
-        # })
-        fire('sample', {'previous': tweet}, delay=0.05)
 
+    except:
+        fire('sample', {'previous': 0.0})
+    else:
+        for w in words(tweet['text']):
+            emit('word', {
+                'action': 'add',
+            'value': (w, 1)
+            })
+            emit('taart', {
+                'action': 'add',
+                'value': (str(w[0]), 1)
+            })
+            emit('balk', {
+                'action': 'add',
+                'value': (str(w[0]), 1)
+            })
+
+    
+        # nicify text
+        text = textwrap.fill(tweet['text'],initial_indent='    ', subsequent_indent='    ')
+        # parse date
+        time = datetime.datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S %z %Y')
+        # generate output
+        output = "[{}] {} (@{}):\n{}".format(time, tweet['user']['name'], tweet['user']['screen_name'], text)
+        
+        ctx.tweetList.append(tweet)
+        emit('message', {'text': "{}".format(output) })
+        fire('sample', {'previous': tweet}, delay=1)
 
 @event('order')
 def order(ctx, e):
@@ -62,17 +93,10 @@ def order(ctx, e):
 
     for tweet in ctx.tweetList:
         if keyword in tweet['text']:
-            # emit('message', {
-            #     'text': "Tweet: {}".format(tweet['text'])
-            # })
-
-            emit('tweet', tweet)
+            emit('message', {
+                'text': "Tweet: {}".format(tweet['text'])
+            })
 
     # emit('message',{
     #     'text': "{}".format("Test submit successed!")
     # })
-
-
-@event('tweet')
-def echo(ctx, e):
-    emit('tweet', e.data)
